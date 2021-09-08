a9d37b343f34acdd0190ee7a73956450ff6fe0e5
Raynaldi <raysnonci@gmail.com>
2021-09-09 06:08:21 +0700

    [D4] Views, Templates, Context

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
+def home(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    # return HttpResponse('oyyyy')
+    return render(request, "leads/home.html", context)
