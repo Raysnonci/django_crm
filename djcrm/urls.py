1a7c812ae0ea3567bafeddbcbe8f9e0b5135a082
Raynaldi <raysnonci@gmail.com>
2021-09-14 11:19:33 +0700

    [DR1] Url

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from leads.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('leads/', include('leads.urls', namespace='leads'))
 ]
---
+++ leads/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from leads.views import *
+
+app_name = 'leads'
+
+urlpatterns = [
+    path('', home)
+]
